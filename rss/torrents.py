e18eb3eafe6c6bade4bd83b3eec10ab93616b264
technoe <technoe@gmail.com>
2020-05-02 00:23:14 -0500

    just maybe

--- rss/torrents.py
+++ rss/torrents.py
@@ -6,19 +6,36 @@ import logging
 import os
 import re
 
-def tdown(title, tname, link):
-  message = "Downloading " + title + " to " + tname
+def get_torrent_title(item):
+  title = item.find('title').text
+  logging.info("set title: " + title)
+  return title
+
+def get_torrent_link(item):
+  link = item.find('link').text
+  logging.info("set link: " + link)
+  return link
+
+def set_filename(item):
+  text = item.find('title').text
+  name = text + ".torrent"
+  logging.info("set filename: " + name)
+  return name
+
+def tdown(title, filename, link):
+  message = "Downloading " + title + " to " + filename
   logging.info(message)
 
   logging.info("requesting uri content")
   r = requests.get(link)
 
   logging.info("write content to file")
-  with open(('/watch/' + tname), 'wb') as f:
+  with open(('/watch/' + filename), 'wb') as f:
     f.write(r.content)
 
-  done = "Torrents should be downloading now!"
+  done = title + " is downloading now to " + filename + ".torrent"
   logging.info(done)
+  return done
 
 def rss(url, matches):
   logmsg = "We're pulling the bookmarks from " + url 
@@ -42,22 +59,21 @@ def rss(url, matches):
         message = "no bookmarks found"
         logging.info(message)
     else:
-      for item in items:
-        title = item.find('title').text
-        logging.info("set title: " + title)
-        link = item.find('link').text
-        logging.info("set link: " + link)
-        tname = title + ".torrent"
-        logging.info("set tname: " + tname)
-        
+      for item in items:        
         if matches:
           for match in matches:
             for i in match:
               pattern = re.compile(i)
+              title = get_torrent_title(item)
               if pattern.search(title):
                 print("match found")
                 print("match title is: " + title)
-                tdown(title, tname, link)
+                filename = set_filename(item)
+                link = get_torrent_link(item)
+                tdown(title, filename, link)
         elif not matches:
-          print(title, tname, link)
-          tdown(title, tname, link)
+          title = get_torrent_title(item)
+          filename = set_filename(item)
+          link = get_torrent_link(item)
+          print(title, filename, link)
+          tdown(title, filename, link)
